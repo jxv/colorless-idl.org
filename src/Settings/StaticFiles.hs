bc1ca5c25f4db94ebe0e0daaa994e7c9e04382ec
jxv <joevargas92@gmail.com>
2018-03-15 15:21:33 -0700

    fluid evolution overview image

--- src/Settings/StaticFiles.hs
+++ src/Settings/StaticFiles.hs
@@ -6,6 +6,7 @@ module Settings.StaticFiles where
 import Settings     (appStaticDir, compileTimeAppSettings)
 import Yesod.Static (staticFiles)
 
+
 -- This generates easy references to files in the static directory at compile time,
 -- giving you compile-time verification that referenced files exist.
 -- Warning: any files added to your static directory during run-time can't be
